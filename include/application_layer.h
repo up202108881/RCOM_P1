5bd2df11b9de81f9f76e37d957f0616064e0aacb
Vasco Moutinho de Oliveira <up202108881@up.pt>
2023-10-22 20:34:24 +0100

    Control and data packet creation

--- include/application_layer.h
+++ include/application_layer.h
@@ -4,6 +4,16 @@
 #ifndef _APPLICATION_LAYER_H_
 #define _APPLICATION_LAYER_H_
 
+// Control packet header size.
+#define CP_HEADER_SIZE 3
+#define DP_HEADER_SIZE 3
+
+#define CP_START 0x02
+#define CP_END 0x03
+#define CP_DATA 0x01
+
+#define CP_T_FILE_SIZE 0
+
 // Application layer main function.
 // Arguments:
 //   serialPort: Serial port name (e.g., /dev/ttyS0).
@@ -14,5 +24,9 @@
 //   filename: Name of the file to send / receive.
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename);
+
+unsigned char * createControlPacket(unsigned char controlField, unsigned int * packetSize);
+
+unsigned char * createDataPacket(unsigned char * data, unsigned int * packetSize);
 
 #endif // _APPLICATION_LAYER_H_
--- src/application_layer.c
+++ src/application_layer.c
@@ -24,6 +24,60 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     int fd = llopen(linkLayer);
     if (fd == -1) printf("DOES NOT WORK! OPEN\n");
     else printf("WORKS! OPEN\n");
-    if (/*linkLayer.role == LLTX &&*/ llclose(fd, linkLayer, FALSE) == -1) printf("DOES NOT WORK! CLOSE\n");
-    else /*if (linkLayer.role == LLTX)*/ printf("WORKS! CLOSE\n");
+    if (llclose(fd, linkLayer, FALSE) == -1) printf("DOES NOT WORK! CLOSE\n");
+    else printf("WORKS! CLOSE\n");
+}
+
+
+unsigned char* createControlPacket(unsigned char controlField, unsigned int * packetSize){
+    switch (controlField)
+    {
+    case 2:
+        unsigned int fileSize = *packetSize;
+        unsigned int fileSizeBytes = 0;
+
+        while(fileSize != 0){
+            fileSizeBytes++;
+            fileSize = fileSize >> 8;
+        }
+        
+        unsigned char* packet =  (unsigned char*)malloc(CP_HEADER_SIZE + fileSizeBytes);
+        packet[0] = controlField;
+        packet[1] = CP_T_FILE_SIZE;
+        packet[2] = fileSizeBytes;
+        for(unsigned int i = 0; i < fileSizeBytes; i++){
+            packet[3 + i] = ((*packetSize) >> (8 * (fileSizeBytes - i - 1))) & 0xFF;
+        }
+        *packetSize = CP_HEADER_SIZE + fileSizeBytes;
+        return packet;
+    
+    case 3:
+        unsigned char* packet[1] = {controlField};
+        return packet;
+    default:
+        printf("Invalid role\n");
+        return NULL;
+    }
+    
+}
+
+unsigned char* createDataPacket(unsigned char * data, unsigned int * packetSize){
+    unsigned int dataSize = *packetSize;
+    unsigned int dataSizeBytes = 0;
+
+    while(dataSize != 0){
+        dataSizeBytes++;
+        dataSize = dataSize >> 8;
+    }
+    
+    unsigned char* packet =  (unsigned char*)malloc(DP_HEADER_SIZE + dataSizeBytes);
+    packet[0] = 1;
+    packet[1] = dataSizeBytes >> 8;
+    packet[2] = dataSizeBytes & 0xFF;
+    
+    for(unsigned int i = 0; i < dataSizeBytes; i++){
+        packet[3 + i] = ((*packetSize) >> (8 * (dataSizeBytes - i - 1))) & 0xFF;
+    }
+    *packetSize = DP_HEADER_SIZE + dataSizeBytes;
+    return packet;
 }
